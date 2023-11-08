f86d1ef8ade7a0a246f41796e78ff74d34d7e9e1
Neha <nehad2000@gmail.com>
2023-11-08 13:48:39 -0600

    working on authenticating

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
+
+export default function App(){
+  const [token, setToken] =useState(null);
 
   return (
     <>
-    
+    <SignUpForm token={token} setToken={setToken}/>
+    <Authenticate token={token} setToken={setToken}/>
     </>
-  )
-
-export default App
+  );
+  
+}
